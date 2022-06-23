d3a1888b8773fdc714dff8754510df90dd1cc891
khpolly <rakpolina@gmail.com>
2022-06-23 18:53:17 +0300

    First lesson

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import ReactDom from "react-dom";
+import App from "./App.jsx";
+
+ReactDom.render(<App/>,document.getElementById("root"));
+
+
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HTMLWebpackPlugin = require("html-webpack-plugin")
+
+
+module.exports = {
+    mode:"development",
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname,"build"),
+        filename: "[name].[contenthash].js",
+        clean: true,
+        publicPath: "/"
+    },
+    devtool: "source-map",
+    devServer: {
+        port: 3000,
+        hot: true,
+        historyApiFallback:{index:'/'},
+        open:true,
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './public/index.html',
+            filename: './index.html'
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+        ]
+    }
+}
